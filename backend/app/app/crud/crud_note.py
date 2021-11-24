ba55e70b5e0c11724f986d563f671ae43b00091d
Enric Franck <enricfranck@gmail.com>
2021-11-24 17:43:03 +0300

    reformulation note

--- backend/app/app/crud/crud_note.py
+++ backend/app/app/crud/crud_note.py
@@ -21,52 +21,54 @@ class CRUDNote(CRUDBase[MatierEC, MatierECCreate, MatierECUpdate]):
         return False
 
 
-    def insert_note(self,db:Session, schema: str,semestre:str,parcours:str, num_carte:str) -> Optional[MatierEC]:
+    def insert_note(self,schema: str,semestre:str,parcours:str, num_carte:str) -> Optional[MatierEC]:
         metadata = MetaData(schema=schema, bind=engine)
         table = Table(f"note_{semestre}_{parcours}", metadata,autoload=True)
+        conn = engine.connect()
         ins = insert(table=table)
         ins = ins.values({f"num_carte:{num_carte}"})
-        db.execute(ins)
-        db.close
+        conn.execute(ins)
         sel = table.select()
-        result = db.execute(sel)
+        result = conn.execute(sel)
         out = result.fetchall()
-        db.close()
+        conn.close()
         return out
 
-    def update_note(self,db:Session,schema: str, semestre:str, parcours:str, num_carte:str, obj_in:str) -> Optional[MatierEC]:
+    def update_note(self,schema: str, semestre:str, parcours:str, num_carte:str, obj_in:str) -> Optional[MatierEC]:
         obj_in_data = jsonable_encoder(obj_in)
         metadata = MetaData(schema=schema, bind=engine)
+        conn = engine.connect()
         table = Table(f"note_{semestre}_{parcours}", metadata,autoload=True)
         up = update(table=table)
         up = up.values(obj_in_data)
         up = up.where(table.c.num_carte == num_carte)
-        db.execute(up)
-        db.close
+        conn.execute(up)
         sel = table.select()
-        result = db.execute(sel)
+        result = conn.execute(sel)
         out = result.fetchall()
-        db.close()
+        conn.close()
         return out
 
-    def read_by_num_carte(self,db:Session,schema: str, semestre:str, parcours:str, num_carte:str) -> Any:
+    def read_by_num_carte(self,schema: str, semestre:str, parcours:str, num_carte:str) -> Any:
         metadata = MetaData(schema=schema, bind=engine)
+        conn = engine.connect()
         table = Table(f"note_{semestre}_{parcours}", metadata,autoload=True)
         sel = table.select()
         sel = sel.where(table.c.num_carte == num_carte)
-        result = db.execute(sel)
+        result = conn.execute(sel)
         out = result.fetchone()
-        db.close()
+        conn.close()
         return out
 
-    def read_ue_moyenne(self,db:Session,schema: str, semestre:str, parcours:str, num_carte:str, obj_in:str) -> Any:
+    def read_ue_moyenne(self,schema: str, semestre:str, parcours:str, num_carte:str, obj_in:str) -> Any:
         metadata = MetaData(schema=schema, bind=engine)
+        conn = engine.connect()
         table = Table(f"note_{semestre}_{parcours}", metadata,autoload=True)
         sel = table.select()
         sel = sel.where(table.c.num_carte == num_carte)
-        result = db.execute(sel)
+        result = conn.execute(sel)
         out = result.fetchone()
-        db.close()
+        conn.close()
         return out
         
 
