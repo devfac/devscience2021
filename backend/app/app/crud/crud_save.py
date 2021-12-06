c351b96e1480b30b61ce8667e40aa2a990c004af
Enric Franck <enricfranck@gmail.com>
2021-12-06 09:27:29 +0300

    add save

---
+++ backend/app/app/crud/crud_save.py
@@ -0,0 +1,40 @@
+import json
+from os import lchown
+from typing import Any, List, Optional
+
+from sqlalchemy import text
+from uuid import UUID
+from sqlalchemy.inspection import inspect
+
+from fastapi.encoders import jsonable_encoder
+from sqlalchemy.orm import Session
+from sqlalchemy import MetaData, Table, column, update, insert,select
+from app.crud.base import CRUDBase
+from app.schemas.matier import MatierECUpdate, MatierECCreate,MatierEC
+from app.db.session import engine
+
+class CRUDSave():
+
+    def insert_data(self,schema: str,semestre:str,parcours:str,session:str ,num_carte:str) :
+        obj_in_data = jsonable_encoder({num_carte})
+        metadata = MetaData(schema=schema, bind=engine)
+        table = Table(f"note_{semestre.lower()}_{parcours.lower()}_{session.lower()}", metadata,autoload=True)
+        conn = engine.connect()
+        ins = insert(table=table)
+        ins = ins.values(obj_in_data)
+        conn.execute(ins)
+        conn.close()
+        
+
+    def read_all_data(self,schema: str,table_name:str) -> List[Any] :
+        metadata = MetaData(schema=schema, bind=engine)
+        table = Table(table_name, metadata,autoload=True)
+        conn = engine.connect()
+        sel = table.select()
+        result = conn.execute(sel)
+        out = result.fetchall()
+        conn.close()
+        return out
+
+
+save = CRUDSave()
