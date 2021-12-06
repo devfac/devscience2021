999a0584f42614b3115f041d9fadaf63439b9cc3
Enric Franck <enricfranck@gmail.com>
2021-12-06 09:26:55 +0300

    add save

---
+++ backend/app/app/api/api_v1/endpoints/save_data.py
@@ -0,0 +1,62 @@
+from typing import Any
+
+from fastapi import APIRouter, Depends
+from pydantic.networks import EmailStr
+
+from app import models, schemas
+from app.api import deps
+from app.core.celery_app import celery_app
+from app.utils import check_table_info, create_anne,check_columns_exist
+from app.excel_code import save_data
+from app import crud
+from sqlalchemy.orm import Session
+
+router = APIRouter()
+
+
+@router.get("/get_models/")
+def get_models(
+    db: Session = Depends(deps.get_db),
+    current_user: models.User = Depends(deps.get_current_active_superuser),
+) -> Any:
+    """
+    """
+    anne_univ = crud.anne_univ.get_multi(db=db)
+    for anne in anne_univ:
+        all_table = check_table_info(create_anne(anne.title))
+        save_data.create_workbook(anne.title,all_table,"models")
+        for table in all_table:
+            colums = check_columns_exist(create_anne(anne.title),table)
+            save_data.write_data_title(anne.title, table,colums,"models")
+            all_data = crud.save.read_all_data(create_anne(anne.title),table)
+            if all_data:
+                save_data.insert_data_xlsx(anne.title,table,all_data,colums,"data")
+    all_table = check_table_info("public")
+    save_data.create_workbook("public",all_table,"models")
+    for table in all_table:
+            colums = check_columns_exist("public",table)
+            save_data.write_data_title("public", table,colums,"models" )
+    return {"msg": "Word received"}
+
+
+@router.get("/save_data/")
+def save_data_to_excel(
+    db: Session = Depends(deps.get_db),
+    current_user: models.User = Depends(deps.get_current_active_superuser),
+) -> Any:
+    """
+    """
+    anne_univ = crud.anne_univ.get_multi(db=db)
+    for anne in anne_univ:
+        all_table = check_table_info(create_anne(anne.title))
+        save_data.create_workbook(anne.title,all_table,"data")
+        for table in all_table:
+            colums = check_columns_exist(create_anne(anne.title),table)
+            save_data.write_data_title(anne.title, table,colums,"data" )
+
+    all_table = check_table_info("public")
+    save_data.create_workbook("public",all_table,"data")
+    for table in all_table:
+            colums = check_columns_exist("public",table)
+            save_data.write_data_title("public", table,colums,"data" )
+    return {"msg": "Word received"}
