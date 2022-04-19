669fad331a2ef0769bb7fdad2e1940afe1d03132
enricfranck <enricfranck@example.com>
2022-04-19 10:04:03 +0300

    note

--- backend/app_file/app/app/schemas/resultat.py
+++
@@ -1,7 +0,0 @@
-from typing import Any, List, Optional
-from pydantic import BaseModel
-
-
-class Resultat(BaseModel):
-    list_valide: Optional[List[Any]]
-    list_non_valide: Optional[List[Any]]
