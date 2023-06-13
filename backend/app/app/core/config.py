ec39b76df04884408614cba7f24d99decc71becf
enricfranck <enricfranck@gmail.com>
2023-06-13 16:22:30 +0300

    fixing bug v2

--- backend/app/app/core/config.py
+++ backend/app/app/core/config.py
@@ -15,9 +15,9 @@ class Settings(BaseSettings):
     # BACKEND_CORS_ORIGINS is a JSON-formatted list of origins
     # e.g: '["http://localhost", "http://localhost:4200", "http://localhost:3000", \
     # "http://localhost:8080", "http://local.dockertoolbox.tiangolo.com"]'
-    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
+    # BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
     LEVEL: List[str] = ["L1", "L2", "L3", "M1", "M2"]
-
+    """
     @validator("BACKEND_CORS_ORIGINS", pre=True)
     def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
         if isinstance(v, str) and not v.startswith("["):
@@ -26,9 +26,9 @@ class Settings(BaseSettings):
             return v
         raise ValueError(v)
 
+    """
     PROJECT_NAME: str
     SENTRY_DSN: Optional[HttpUrl] = None
-
     @validator("SENTRY_DSN", pre=True)
     def sentry_dsn_can_be_blank(cls, v: str) -> Optional[str]:
         if len(v) == 0:
